f957431b1ffccc9d23492a8a7e0b64a11f7b8272
Yan Ming Leung <joshualeung15@gmail.com>
2024-11-04 14:34:32 -0800

    Fix test cases expected output 39-45

--- adjusttxt/test/edu/gatech/seclass/adjusttxt/MyMainTest.java
+++ adjusttxt/test/edu/gatech/seclass/adjusttxt/MyMainTest.java
@@ -912,7 +912,7 @@ public class MyMainTest {
     @Test
     public void adjusttxtTest39() {
         String input = "   Write your name.   " + System.lineSeparator() + "   Write your name   " + System.lineSeparator();
-        String expectedOutput = "Prefixname. your Write" + System.lineSeparator();
+        String expectedOutput = "Prefix   name your Write " + System.lineSeparator();
 
         Path inputFile = createFile(input);
 
@@ -938,7 +938,7 @@ public class MyMainTest {
     @Test
     public void adjusttxtTest40() {
         String input = "   Write your name." + System.lineSeparator() + "   Write your name" + System.lineSeparator();
-        String expectedOutput = "Prefix   name. your Write" + System.lineSeparator();
+        String expectedOutput = "Prefixname your Write " + System.lineSeparator();
 
         Path inputFile = createFile(input);
 
@@ -963,7 +963,7 @@ public class MyMainTest {
     @Test
     public void adjusttxtTest41() {
         String input = "   Write your name." + System.lineSeparator() + "   Write your name" + System.lineSeparator();
-        String expectedOutput = "Prefix   name. your Write" + System.lineSeparator();
+        String expectedOutput = "Prefixname your Write " + System.lineSeparator();
 
         Path inputFile = createFile(input);
 
@@ -989,7 +989,7 @@ public class MyMainTest {
     @Test
     public void adjusttxtTest42() {
         String input = "   Write your name." + System.lineSeparator() + "   Write your name" + System.lineSeparator();
-        String expectedOutput = "Prefix   name. your Write" + System.lineSeparator();
+        String expectedOutput = "Prefixname your Write " + System.lineSeparator();
 
         Path inputFile = createFile(input);
 
@@ -1014,7 +1014,7 @@ public class MyMainTest {
     @Test
     public void adjusttxtTest43() {
         String input = "   Write your name." + System.lineSeparator() + "   Write your name" + System.lineSeparator();
-        String expectedOutput = "Prefix.eman ruoy etirW" + System.lineSeparator();
+        String expectedOutput = "Prefixeman ruoy etirW " + System.lineSeparator();
 
         Path inputFile = createFile(input);
 
@@ -1040,7 +1040,7 @@ public class MyMainTest {
     @Test
     public void adjusttxtTest44() {
         String input = "   Write your name." + System.lineSeparator() + "   Write your name" + System.lineSeparator();
-        String expectedOutput = "Prefix.eman ruoy etirW" + System.lineSeparator();
+        String expectedOutput = "Prefixeman ruoy etirW " + System.lineSeparator();
 
         Path inputFile = createFile(input);
 
@@ -1065,7 +1065,7 @@ public class MyMainTest {
     @Test
     public void adjusttxtTest45() {
         String input = "   Write your name." + System.lineSeparator() + "   Write your name" + System.lineSeparator();
-        String expectedOutput = "Prefix.eman ruoy etirW" + System.lineSeparator();
+        String expectedOutput = "Prefixeman ruoy etirW " + System.lineSeparator();
 
         Path inputFile = createFile(input);
 
