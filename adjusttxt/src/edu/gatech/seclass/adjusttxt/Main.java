7a760e79718e3882cadfa1fd2601a82b2898f1c3
Yan Ming Leung <joshualeung15@gmail.com>
2024-11-11 14:03:41 -0800

    [WIP] Create arraylist to store arguments

--- adjusttxt/src/edu/gatech/seclass/adjusttxt/Main.java
+++ adjusttxt/src/edu/gatech/seclass/adjusttxt/Main.java
@@ -1,11 +1,23 @@
 package edu.gatech.seclass.adjusttxt;
-
+import java.util.ArrayList;
 public class Main {
     // Empty Main class for compiling Individual Project
     // During Deliverable 1 and Deliverable 2, DO NOT ALTER THIS CLASS or implement it
 
     public static void main(String[] args) {
-        // Empty Skeleton Method
+        // if argument is empty --> print error message
+        if (args.length == 0) {
+            usage();
+        }
+
+        // if not empty --> Create an array list to store the args
+        ArrayList<String> argsList = new ArrayList<>();
+
+        // Add each of the argument to the list
+        for (int i = 0; i < args.length; i++) {
+            argsList.add(args[i]);
+        }
+
     }
 
     private static void usage() {
