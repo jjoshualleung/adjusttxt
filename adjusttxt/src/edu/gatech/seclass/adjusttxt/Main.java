7ff0d34190b1de7ba8a954a47e505bcd20ca1b23
Yan Ming Leung <joshualeung15@gmail.com>
2024-11-17 15:58:47 -0800

    Revert back to origin

--- adjusttxt/src/edu/gatech/seclass/adjusttxt/Main.java
+++ adjusttxt/src/edu/gatech/seclass/adjusttxt/Main.java
@@ -300,11 +300,11 @@ public class Main {
         List<String> result = new ArrayList<>();
 
         for (String line : outputLines) {
-            String[] words = line.split("(?<=\\t)|(?=\\t)| ");
+            String[] words = line.split(" ");
             StringBuilder reversedLine = new StringBuilder();
             for (int word = words.length - 1; word >= 0; word--) {
                 reversedLine.append((words[word]));
-                if (word != 0 && !words[word].equals("\t")) {
+                if (word != 0) {
                     reversedLine.append(" ");
                 }
             }
