e80196dc0013ba95e4a2a5c60e7e92db40d82205
Yan Ming Leung <joshualeung15@gmail.com>
2024-11-17 00:39:18 -0800

    Fix replace empty space method

--- adjusttxt/src/edu/gatech/seclass/adjusttxt/Main.java
+++ adjusttxt/src/edu/gatech/seclass/adjusttxt/Main.java
@@ -265,7 +265,7 @@ public class Main {
         for (String line : outputLines) {
             // if true then proceed
             // Remove all the space
-            result.add(line.replace(" ", ""));
+            result.add(line.replaceAll("\\s+", ""));
         }
         return result;
     }
