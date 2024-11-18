f3b48a1fa5517810d8e7b6a2f8ca0873b5d92533
Yan Ming Leung <joshualeung15@gmail.com>
2024-11-18 09:48:53 -0800

    Initial commit of d4 files

---
+++ adjusttxt/src/edu/gatech/seclass/adjusttxt/AdjustTxtException.java
@@ -0,0 +1,13 @@
+package edu.gatech.seclass.adjusttxt;
+
+/** Signals that an error occurred when running AdjustTxt. */
+public class AdjustTxtException extends Exception {
+    /**
+     * Constructs a AdjustTxtException with the specified message describing the error.
+     *
+     * @param s the error message
+     */
+    AdjustTxtException(String s) {
+        super(s);
+    }
+}
