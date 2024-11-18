967ed9708ee3ffde89cd8fb442e282fd0afa5df0
Yan Ming Leung <joshualeung15@gmail.com>
2024-11-18 10:23:32 -0800

    Create adjusttxt class AdjustTxtInterface

---
+++ adjusttxt/src/edu/gatech/seclass/adjusttxt/AdjustTxt.java
@@ -0,0 +1,44 @@
+package edu.gatech.seclass.adjusttxt;
+
+public class AdjustTxt implements AdjustTxtInterface{
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setFilepath(String filepath) {
+
+    }
+
+    @Override
+    public void setLineToSkip(LineToSkip lineToSkip) {
+
+    }
+
+    @Override
+    public void setRemoveSpaces(RemoveSpaces removeSpaces) {
+
+    }
+
+    @Override
+    public void setRemoveEmptyLines(boolean removeEmptyLines) {
+
+    }
+
+    @Override
+    public void setReverseLine(ReverseLine reverseLine) {
+
+    }
+
+    @Override
+    public void setPrefix(String prefix) {
+
+    }
+
+    @Override
+    public void adjusttxt() throws AdjustTxtException {
+
+    }
+}
