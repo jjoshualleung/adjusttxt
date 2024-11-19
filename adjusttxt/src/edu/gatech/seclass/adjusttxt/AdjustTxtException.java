defa790c52729b724f1c12d09e1165965061bddf
Yan Ming Leung <joshualeung15@gmail.com>
2024-11-19 10:27:09 -0800

    Revert adusttxt exception

--- adjusttxt/src/edu/gatech/seclass/adjusttxt/AdjustTxtException.java
+++ adjusttxt/src/edu/gatech/seclass/adjusttxt/AdjustTxtException.java
@@ -10,8 +10,4 @@ public class AdjustTxtException extends Exception {
     AdjustTxtException(String s) {
         super(s);
     }
-
-    AdjustTxtException() {
-        super("Usage: adjusttxt [ -s number | -w spacing | -x | -r target | -p prefix ] FILE");
-    }
 }
